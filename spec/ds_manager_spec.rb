6f2495be22dc0ef10b71c7c5f1de1f094415b9f2
Cedric Villa <cedric.villa@gmail.com>
2015-07-14 13:46:50 +0200

    add context

--- spec/ds_manager_spec.rb
+++ spec/ds_manager_spec.rb
@@ -10,9 +10,11 @@ describe 'The HelloWorld App' do
     Sinatra::Application
   end
 
-  it "says hello" do
-    get '/'
-    expect(last_response).to be_ok
-    # expect(last_response.body).to eq('Hello World')
+  context 'when I visit /' do
+    it "should respond ok" do
+      get '/'
+      expect(last_response).to be_ok
+      # expect(last_response.body).to eq('Hello World')
+    end
   end
 end
