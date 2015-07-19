2d1c99e5ceb10d7f6ee47534830820fffdc8e670
Cedric Villa <cedric.villa@gmail.com>
2015-07-19 15:10:54 +0200

    fix test feedback

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,8 @@
 docker-compose build
 docker-compose up -d
 # sleep 1
-docker exec -t -i datasourcemanager_datasourcemanager_1 rspec
+# docker exec -t -i datasourcemanager_datasourcemanager_1 rspec
+RESULT=$?
 docker-compose stop
+[ $RESULT -ne 0 ] && exit 1
+exit 0
