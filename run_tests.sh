f788eb9f13c97372fc81bf9192935e55cf0c739d
Cedric Villa <cedric.villa@gmail.com>
2015-07-19 15:21:39 +0200

    add actual tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,9 @@
 # Build service with dependencies and run tests
 docker-compose build
 docker-compose up -d
-# sleep 1
-# docker exec -t -i datasourcemanager_datasourcemanager_1 rspec
+sleep 1
+docker exec -t datasourcemanager_datasourcemanager_1 rspec
+# docker exec -ti datasourcemanager_datasourcemanager_1 script -q -c "rspec" /dev/null
 RESULT=$?
 docker-compose stop
 [ $RESULT -ne 0 ] && exit 1
